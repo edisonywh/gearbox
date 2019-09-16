2238c17e8e55728a90383dd2a7f69f94a7991f62
Edison Yap <edisonywh@gmail.com>
2019-09-16 23:14:21 +0800

    Initial commit

---
+++ lib/gearbox.ex
@@ -0,0 +1,18 @@
+defmodule Gearbox do
+  @moduledoc """
+  Documentation for Gearbox.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Gearbox.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/gearbox_test.exs
@@ -0,0 +1,8 @@
+defmodule GearboxTest do
+  use ExUnit.Case
+  doctest Gearbox
+
+  test "greets the world" do
+    assert Gearbox.hello() == :world
+  end
+end
