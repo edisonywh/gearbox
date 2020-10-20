038807e83c401d019903a2ae7ca02e348d5c67b7
Edison Yap <edisonywh@gmail.com>
2020-10-20 20:38:21 +0200

    Bump version to 0.3.0
    This adds support for Gearbox.Ecto.transition_changeset/3`.
    
    https://github.com/edisonywh/gearbox/pull/10
    
    Fixes #6

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Gearbox.MixProject do
   def project do
     [
       app: :gearbox,
-      version: "0.2.1",
+      version: "0.3.0",
       elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
