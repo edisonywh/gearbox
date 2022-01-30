1c90d89f92b28cbf0d1e8d88945c7dfc5303f3b3
Edison Yap <edisonywh@gmail.com>
2022-01-30 20:05:01 +0800

    Bump version to 0.3.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Gearbox.MixProject do
   def project do
     [
       app: :gearbox,
-      version: "0.3.1",
+      version: "0.3.2",
       elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
